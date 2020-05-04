9e49dcc786550a4e5d589eb1d168e9e8b83ded9f
leseul-yj <79645550@qq.com>
2020-05-04 23:57:32 +0800

    管理给服务发送请求

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io");
 const bodyParser = require('body-parser');
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname,'src')));
 
 // 解析 application/json
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 // 解析 application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded());
 
 // socketIO 
 const socketIO = io(http);
 let timer;
-socketIO.on("connection", socket => {
+socketIO.on("connection",socket => {
     // 和客户端商量的频道 
-    socket.on("message", msg => {
+    socket.on("message",msg => {
         console.log(msg);
-        socketIO.emit("message", "接受到客户端推送的消息后 回消息");
+        socketIO.emit("message","接受到客户端推送的消息后 回消息");
     });
 
     timer = setInterval(() => {
-        socketIO.emit("message", Math.random());
-    }, 600000);
+        socketIO.emit("message",Math.random());
+    },600000);
 
-    socket.on('error', (err) => {
+    socket.on('error',(err) => {
         console.log(err);
     });
 
-    socket.on("disconnect", _ => { // 客户端断开链接
+    socket.on("disconnect",_ => { // 客户端断开链接
         clearInterval(timer);
         console.log("客户端关闭链接")
-        socketIO.emit("message", "链接关闭");
+        socketIO.emit("message","链接关闭");
     });
 })
 
@@ -42,21 +42,23 @@ var jsonParser = bodyParser.json();
 
 //创建application/x-www-form-urlencoded
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-
-app.get('/',  (req, res) => {
+app.use(urlencodedParser);
+app.use(jsonParser);
+app.get('/',(req,res) => {
     res.send('index.html'); //发送一个请求
 })
 
-app.get('/websocket', (req, res) => {
-    res.send('index.html'); 
+app.get('/websocket',(req,res) => {
+    res.send('index.html');
 })
-app.post('/addNewMes',jsonParser, (req, res) => {
-    socketIO.emit("message", req.body);
+app.post('/addNewMes',jsonParser,(req,res) => {
+    console.log(req.body)
+    socketIO.emit("message",req.body);
 })
 // app.get('/management', function (req, res) {
 //     res.sendFile(path.join(__dirname, '/src/websocket/management/index.html'));
 // })
 
-http.listen(5555, function () { //在3000端口启动
+http.listen(5555,function() { //在3000端口启动
     console.log('Example app listening on port 5555');
 })
--- src/websocket/management/index.js
+++ src/websocket/management/index.js
@@ -2,7 +2,7 @@
 let addMesgText = document.getElementById("addMesg");
 let btnSubit = document.getElementById("btnSubmit");
 
-btnSubit.addEventListener('click', function () {
+btnSubit.addEventListener('click',function() {
     // let postData = {
     //     createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
     //     message: addMesgText.textContent
@@ -11,9 +11,9 @@ btnSubit.addEventListener('click', function () {
         createTime: '2020-04-29 13:56:00',
         message: addMesgText.value
     };
-    fetch("/addNewMes", {
+    fetch("/addNewMes",{
         method: "POST",
-        data: JSON.stringify(postData),
+        bddy: JSON.stringify(postData),
     }).then((res) => {
 
     })
