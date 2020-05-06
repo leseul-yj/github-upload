754226a4c23ce1602d6521be100d8451991687b5
leseul-yj <79645550@qq.com>
2020-05-06 11:18:21 +0800

    收到的request.body是空的问题

--- src/websocket/management/index.js
+++ src/websocket/management/index.js
@@ -2,7 +2,7 @@
 let addMesgText = document.getElementById("addMesg");
 let btnSubit = document.getElementById("btnSubmit");
 
-btnSubit.addEventListener('click',function() {
+btnSubit.addEventListener('click', function () {
     // let postData = {
     //     createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
     //     message: addMesgText.textContent
@@ -11,9 +11,13 @@ btnSubit.addEventListener('click',function() {
         createTime: '2020-04-29 13:56:00',
         message: addMesgText.value
     };
-    fetch("/addNewMes",{
+    fetch("/addNewMes", {
         method: "POST",
-        bddy: JSON.stringify(postData),
+        // 不添加header 服务器那边收到的是空
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: JSON.stringify(postData),
     }).then((res) => {
 
     })
