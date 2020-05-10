72fda2c574877a692e2561c43978a2e50dd55f77
leseul-yj <79645550@qq.com>
2020-05-10 23:58:00 +0800

    添加notification通知

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,3 +1,14 @@
+// 请求权限 Notification.requestPermission(CALLBACK)
+// 应用发送通知之前必须要发送通知权限，才能成功进行通知，这个方法支持then方式的链式调用，可以一步调用
+
+
+const permission = Notification.permission;
+if(permission == 'granted') {
+    alert("grunt");
+} else {
+    alert("denied")
+}
+
 let serverMes = document.getElementById("serverMes");
 let clientMes = document.getElementById("clientMes");
 // function socketConnect(url) {
@@ -22,20 +33,27 @@ let clientMes = document.getElementById("clientMes");
 
 let socket = io.connect('http://127.0.0.1:5555');
 
-socket.on('connect', function () {
+socket.on('connect',function() {
     console.log('连接成功');
 });
-console.log('socket', socket);
+console.log('socket',socket);
 
-socket.on('message', result => {
+socket.on('message',result => {
     result = JSON.parse(result);
-    if (result.status === "false") return;
+    if(result.status === "false") return;
     const {
         data
     } = result;
     clientMes.innerHTML += `<p class="wsReceiveMes"><span class="text">${data.message}</span><span class="time">${data.createTime}</span></p>`;
 });
 
-socket.on('disconnect', () => {
+socket.on('disconnect',() => {
     //clearInterval(interval);
 });
+
+//使用notification通知
+//1。获取用户权限 Notification.permission是一个静态方法，返回一个string，可以根据
+// 返回值判断用户是否授予了通知权限， 返回值有三种
+// default 用户还未被询问是否授权，所以通知不会被显示
+// granted 表示已经询问过用户，并且用户已经授权了显示通知的权限
+// denied 用户已经明确拒绝了显示通知的权限
